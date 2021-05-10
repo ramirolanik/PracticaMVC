b3a7de58b5097493572a1e610a1c242e1f5fc13a
ramirolanik <57732298+ramirolanik@users.noreply.github.com>
2021-05-10 00:01:22 -0300

    Add files via upload

---
+++ ClassLibrary1.Logic/CategoriesLogic.cs
@@ -0,0 +1,30 @@
+﻿using ClassLibrary1.Data;
+using ClassLibrary1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Logic
+{
+    public class CategoriesLogic
+    {
+        private readonly Model2 context;
+
+        public CategoriesLogic()
+        {
+            context = new Model2();
+
+        }
+
+        public List<Categories> GetAll()
+        {
+
+            return context.Categories.ToList();
+
+        }
+
+
+    }
+}
---
+++ ClassLibrary1.Logic/CustomersLogic.cs
@@ -0,0 +1,60 @@
+﻿using ClassLibrary1.Data;
+using ClassLibrary1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Logic
+{
+    public class CustomersLogic
+    {
+        private readonly Model1 context;
+
+        public CustomersLogic()
+        {
+            context = new Model1();
+
+        }
+        
+        public List<Customers> GetAll()
+        {
+
+            return context.Customers.ToList();
+
+        }
+
+        public void Add(Customers newCustomer)
+        {
+            context.Customers.Add(newCustomer);
+
+            context.SaveChanges();
+
+
+        }
+
+        public void Delete(string id)
+        {
+            var customerEliminar = context.Customers.First(c => c.CustomerID == id);
+
+            context.Customers.Remove(customerEliminar);
+
+            context.SaveChanges();
+        }
+
+        public void Update(Customers customers)
+        {
+            var customerUpdate = context.Customers.Find(customers.CustomerID);
+
+            customerUpdate.ContactName = customers.ContactName;
+
+            context.SaveChanges();
+
+
+        
+
+        }
+
+    }
+}
